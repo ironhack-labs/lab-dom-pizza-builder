d836d529629badde02ecec9046314226d6b099aa
Ryan Kegerreis <ryankegerreis@icloud.com>
2019-04-08 16:45:46 -0400

    Prices work.

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -13,8 +13,8 @@ var state = {
   pepperonni: true,
   mushrooms: true,
   greenPeppers: true,
-  whiteSauce: true,
-  glutenFreeCrust: true
+  whiteSauce: false,
+  glutenFreeCrust: false
 }
 // This function takes care of rendering the pizza based on the state
 // This function is triggered once at the begining and everytime the state is changed
@@ -27,9 +27,10 @@ function renderEverything() {
   renderPrice()
 }
 function renderPepperonni() {
-  document.querySelectorAll('.pep').forEach(function($pep){
+  document.querySelectorAll('.pep ').forEach(function($pep){
     if (state.pepperonni) {
       $pep.style.visibility = "visible";
+
     }
     else {
       $pep.style.visibility = "hidden";
@@ -87,9 +88,45 @@ function renderButtons() {
   // Iteration 3: add/remove the class "active" of each `<button class="btn">`
 }
 
-function renderPrice() {
+
+// function getTotal(){
+  function renderPrice() {
+  let sum=basePrice;
+  let prices = ''
+  for(let key in state){
+    if (state[key]){
+      //console.log('key',key, 'state[key]',state[key])
+      console.log('we have ',key)
+      //basePrice
+      console.log(ingredients[key].price)
+      prices+=`<li>${ingredients[key].name}</li>`
+      sum+=ingredients[key].price
+    }
+    document.querySelectorAll(".total-price")[0].innerHTML = "$" + sum;
+    console.log("Sum is "+sum)
+  }}
+
+  // ==== Kenneths Function ====
+  // function renderPrice() {
+
+  //   let total = 10;
+  //   $(`aside li:visible`).each(function() {
+  //     // let price = Number($(this).text().split(' ‘)[0].replace(‘$’, ‘’) )
+  //     let price = Number(this.innerText[1]);
+  //     total += price;
+  //   });
+  //   $(`strong`)[0].innerText = `$` + total;
+  //   }
+
+
+  // $("aside li:visible").each(function())
+  // $(`strong`)[0].innerText=`$` + sum;
+  // console.log("Sum is " +sum)
+
+
+//$(`aside)
   // Iteration 4: change the HTML of `<aside class="panel price">`
-}
+// }
 renderEverything()
 renderButtons()
 
