fafeeb115d29c57caeefbe7a3f4c58a9b9ad1035
Your Name <josedelacruz@Joses-MacBook-Air.local>
2019-06-29 18:02:44 -0400

    done

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,85 +1,74 @@
 // Write your Pizza Builder JavaScript in this file.
+$(document).ready(function(){
 
-// Constants 
-var basePrice = 10
-var ingredients = {
-  pepperonni: {name: 'Pepperonni', price: 1},
-  mushrooms: {name: 'Mushrooms', price: 1},
-  greenPeppers: {name: 'Green Peppers', price: 1},
-  whiteSauce: {name: 'White sauce', price: 3},
-  glutenFreeCrust: {name: 'Gluten-free crust', price: 5}
-}
+const ingredients = [
+{name:"pepperonni", price: 1, active: true},
+{name:"mushrooms", price: 1, active: true},
+{name:"green peppers", price: 1, active: true},
+{name:"white sauce", price: 3, active: true},
+{name:"gluten-free crust", price: 5, active: true}
+]
+  
+function theIngredientsList(){
+  let listedPrices = $('aside.price > ul');
 
-// Initial value of the state (the state values can change over time)
-var state = {
-  pepperonni: true,
-  mushrooms: true,
-  greenPeppers: true,
-  whiteSauce: false,
-  glutenFreeCrust: false
-}
-
-// This function takes care of rendering the pizza based on the state
-// This function is triggered once at the begining and everytime the state is changed
-function renderEverything() {
-  renderPepperonni()
-  renderMushrooms()
-  renderGreenPeppers()
-  renderWhiteSauce()
-  renderGlutenFreeCrust()
-
-  renderButtons()
-  renderPrice()
-}
-
-function renderPepperonni() {
-  document.querySelectorAll('.pep').forEach(function($pep){
-    if (state.pepperonni) {
-      $pep.style.visibility = "visible";
+  listedPrices.empty();
+  let total = 10;
+  ingredients.forEach(oneIngredient => {
+    if(oneIngredient.active){
+      let newIng = $('<li></li>');
+      newIng.html(`$${oneIngredient.price} ${oneIngredient.name}`)
+      listedPrices.append(newIng);
+      total += oneIngredient.price;
     }
-    else {
-      $pep.style.visibility = "hidden";
-    }
-  })
-}
+  });
+  $('#cart').html("$"+total);
+}  
 
-function renderMushrooms() {
-  // Iteration 1: set the visibility of `<section class="mushroom">`
-}
-
-function renderGreenPeppers() {
-  // Iteration 1: set the visibility of `<section class="green-pepper">`
-}
-
-function renderWhiteSauce() {
-  // Iteration 2: add/remove the class "sauce-white" of `<section class="sauce">`
-}
-
-function renderGlutenFreeCrust() {
-  // Iteration 2: add/remove the class "crust-gluten-free" of `<section class="crust">`
-}
-
-function renderButtons() {
-  // Iteration 3: add/remove the class "active" of each `<button class="btn">`
-}
-
-function renderPrice() {
-  // Iteration 4: change the HTML of `<aside class="panel price">`
-}
-
-
-renderEverything()
 
 // Iteration 1: Example of a click event listener on `<button class="btn btn-pepperonni">`
-document.querySelector('.btn.btn-pepperonni').onclick = function() {
-  state.pepperonni = !state.pepperonni
-  renderEverything()
-}
+let pepperonniButton = $('.btn-pepperonni');
+pepperonniButton.click(function(){
+  $('.pep').toggle();
+  $('.btn-pepperonni').toggleClass('active');
+  ingredients[0].active = !ingredients[0].active;
+})
 
 // Iteration 1: Add click event listener on `<button class="btn btn-mushrooms">`
+let mushroomButton = $('.btn-mushrooms');
+mushroomButton.click(function(){
+  $('.mushroom').toggle();
+  $('.btn-mushrooms').toggleClass('active');
+  ingredients[1].active = !ingredients[1].active;
+})
 
 // Iteration 1: Add click event listener on `<button class="btn btn-green-peppers">`
+let greenPepperButton = $('.btn-green-peppers');
+greenPepperButton.click(function(){
+  $('.green-pepper').toggle();
+  $('.btn-green-peppers').toggleClass('active');
+  ingredients[2].active = !ingredients[2].active;
+})
 
 // Iteration 2: Add click event listener on `<button class="btn btn-sauce">`
+let whiteSauceButton = $('.btn-sauce');
+whiteSauceButton.click(function(){
+  $('.sauce').toggleClass('sauce-white');
+  $('.btn-sauce').toggleClass('active');
+  ingredients[3].active = !ingredients[3].active;
+})
 
 // Iteration 2: Add click event listener on `<button class="btn btn-crust">`
+let glutenCrustButtton = $('.btn-crust');
+glutenCrustButtton.click(function(){
+  $('.crust').toggleClass('crust-gluten-free');
+  $('.btn-crust').toggleClass('active');
+  ingredients[4].active = !ingredients[4].active;
+})
+
+//Iteration 3: Add active click event.
+$('.btn').click(function() {
+  theIngredientsList();
+})
+
+})
