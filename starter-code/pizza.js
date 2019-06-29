e55a724dbe8e2fb0250acb8a3e1ebbbd93ac341f
Fabricio Policarpo <fabriciopolicarpo0@gmail.com>
2019-06-29 15:51:29 -0400

    done

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -4,15 +4,7 @@
 
 // Write your Pizza Builder JavaScript in this file.
 
-// Constants 
-var basePrice = 10
-var ingredients = {
-  pepperonni: { name: 'Pepperonni', price: 1 },
-  mushrooms: { name: 'Mushrooms', price: 1 },
-  greenPeppers: { name: 'Green Peppers', price: 1 },
-  whiteSauce: { name: 'White sauce', price: 3 },
-  glutenFreeCrust: { name: 'Gluten-free crust', price: 5 }
-}
+
 
 // Initial value of the state (the state values can change over time)
 var state = {
@@ -31,39 +23,37 @@ function renderEverything() {
   renderGreenPeppers()
   renderWhiteSauce()
   renderGlutenFreeCrust()
-
   renderButtons()
   renderPrice()
 }
 
 
-$('.btn').click(function () {
-  $(this).toggleClass('active')
-})
 
-// function renderPepperonni() {
-//   document.querySelectorAll('.pep').forEach(function ($pep) {
-//     if (state.pepperonni) {
-//       $pep.style.visibility = "visible";
-//     }
-//     else {
-//       $pep.style.visibility = "hidden";
-//     }
-//   })
-// }
+const ingredients = [
+  { name: "pepperonni", price: 1, active: true },
+  { name: "mushrooms", price: 1, active: true },
+  { name: "green peppers", price: 1, active: true },
+  { name: "white sauce", price: 3, active: true },
+  { name: "gluten-free crust", price: 5, active: true }
+]
 
-let thePrice = parseInt($('#list').next().html().slice(1))
+function drawEntireIngredientsList() {
+  let theListOnThePage = $('aside.price > ul');
 
-// let thePrice = parseInt($('#panel').children('b').html().slice(1))
+  theListOnThePage.empty();
+  let total = 10;
+  ingredients.forEach(oneIngredient => {
+    if (oneIngredient.active) {
+      let newLi = $('<li></li>');
+      newLi.html(`$${oneIngredient.price} ${oneIngredient.name}`)
+      theListOnThePage.append(newLi);
+      total += oneIngredient.price;
+    }
 
+  });
+  $('#totes').html("$" + total);
+}
 
-
-
-
-
-
-let total = $('#panel > strong').html().slice(1)
-console.log(total)
 
 
 
@@ -71,7 +61,8 @@ function renderPepperonni() {
 
   $('.pep').toggle(1000)
   let listItem = $('#list>li:first-child')
-  listItem.toggle(1000)
+  // listItem.toggle(1000)
+  ingredients[0].active = !ingredients[0].active
 
 }
 
@@ -80,7 +71,8 @@ function renderPepperonni() {
 function renderMushrooms() {
   // Iteration 1: set the visibility of `<section class="mushroom">`
   $('.mushroom').toggle(1000)
-  $('#list>li:nth-child(2)').toggle(1000)
+  // $('#list>li:nth-child(2)').toggle(1000)
+  ingredients[1].active = !ingredients[1].active
 
 }
 
@@ -88,19 +80,21 @@ function renderGreenPeppers() {
   // Iteration 1: set the visibility of `<section class="green-pepper">`
   $('.green-pepper').toggle(1000)
   $('#list>li:nth-child(3)').toggle(1000)
+  ingredients[2].active = !ingredients[2].active
 }
 
 function renderWhiteSauce() {
   // Iteration 2: add/remove the class "sauce-white" of `<section class="sauce">`
   $('section.sauce').toggleClass('sauce-white')
-  $('#list>li:nth-child(4)').toggle(1000)
+  // $('#list>li:nth-child(4)').toggle(1000)
+  ingredients[3].active = !ingredients[3].active
 
 }
 
 function renderGlutenFreeCrust() {
   // Iteration 2: add/remove the class "crust-gluten-free" of `<section class="crust">`
   $('section.crust').toggleClass('crust-gluten-free')
-  $('#list>li:nth-child(5)').toggle(1000)
+  ingredients[4].active = !ingredients[4].active
 }
 
 function renderButtons() {
@@ -115,7 +109,7 @@ function renderPrice() {
 
 renderEverything()
 
-// Iteration 1: Example of a click event listener on `<button class="btn btn-pepperonni">`
+
 // document.querySelector('.btn.btn-pepperonni').onclick = function () {
 //   state.pepperonni = !state.pepperonni
 
@@ -134,3 +128,10 @@ $('.btn.btn-sauce').click(renderWhiteSauce)
 
 // Iteration 2: Add click event listener on `<button class="btn btn-crust">`
 $('.btn.btn-crust').click(renderGlutenFreeCrust)
+
+
+$('.btn').click(function () {
+  $(this).toggleClass('active')
+  drawEntireIngredientsList();
+
+})
