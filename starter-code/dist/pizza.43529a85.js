74d94442c10d186d1a0665f3a443d5961f4d8265
Adamy <viniciusbohreradamy@gmail.com>
2019-09-10 14:50:51 -0300

    Initial commit

---
+++ starter-code/dist/pizza.43529a85.js
@@ -0,0 +1,484 @@
+// modules are defined as an array
+// [ module function, map of requires ]
+//
+// map of requires is short require name -> numeric require
+//
+// anything defined in a previous bundle is accessed via the
+// orig method which is the require for previous bundles
+parcelRequire = (function (modules, cache, entry, globalName) {
+  // Save the require from previous bundle to this closure if any
+  var previousRequire = typeof parcelRequire === 'function' && parcelRequire;
+  var nodeRequire = typeof require === 'function' && require;
+
+  function newRequire(name, jumped) {
+    if (!cache[name]) {
+      if (!modules[name]) {
+        // if we cannot find the module within our internal map or
+        // cache jump to the current global require ie. the last bundle
+        // that was added to the page.
+        var currentRequire = typeof parcelRequire === 'function' && parcelRequire;
+        if (!jumped && currentRequire) {
+          return currentRequire(name, true);
+        }
+
+        // If there are other bundles on this page the require from the
+        // previous one is saved to 'previousRequire'. Repeat this as
+        // many times as there are bundles until the module is found or
+        // we exhaust the require chain.
+        if (previousRequire) {
+          return previousRequire(name, true);
+        }
+
+        // Try the node require function if it exists.
+        if (nodeRequire && typeof name === 'string') {
+          return nodeRequire(name);
+        }
+
+        var err = new Error('Cannot find module \'' + name + '\'');
+        err.code = 'MODULE_NOT_FOUND';
+        throw err;
+      }
+
+      localRequire.resolve = resolve;
+      localRequire.cache = {};
+
+      var module = cache[name] = new newRequire.Module(name);
+
+      modules[name][0].call(module.exports, localRequire, module, module.exports, this);
+    }
+
+    return cache[name].exports;
+
+    function localRequire(x){
+      return newRequire(localRequire.resolve(x));
+    }
+
+    function resolve(x){
+      return modules[name][1][x] || x;
+    }
+  }
+
+  function Module(moduleName) {
+    this.id = moduleName;
+    this.bundle = newRequire;
+    this.exports = {};
+  }
+
+  newRequire.isParcelRequire = true;
+  newRequire.Module = Module;
+  newRequire.modules = modules;
+  newRequire.cache = cache;
+  newRequire.parent = previousRequire;
+  newRequire.register = function (id, exports) {
+    modules[id] = [function (require, module) {
+      module.exports = exports;
+    }, {}];
+  };
+
+  var error;
+  for (var i = 0; i < entry.length; i++) {
+    try {
+      newRequire(entry[i]);
+    } catch (e) {
+      // Save first error but execute all entries
+      if (!error) {
+        error = e;
+      }
+    }
+  }
+
+  if (entry.length) {
+    // Expose entry point to Node, AMD or browser globals
+    // Based on https://github.com/ForbesLindesay/umd/blob/master/template.js
+    var mainExports = newRequire(entry[entry.length - 1]);
+
+    // CommonJS
+    if (typeof exports === "object" && typeof module !== "undefined") {
+      module.exports = mainExports;
+
+    // RequireJS
+    } else if (typeof define === "function" && define.amd) {
+     define(function () {
+       return mainExports;
+     });
+
+    // <script>
+    } else if (globalName) {
+      this[globalName] = mainExports;
+    }
+  }
+
+  // Override the current require with this new one
+  parcelRequire = newRequire;
+
+  if (error) {
+    // throw error from earlier, _after updating parcelRequire_
+    throw error;
+  }
+
+  return newRequire;
+})({"pizza.js":[function(require,module,exports) {
+// Write your Pizza Builder JavaScript in this file.
+// Constants 
+var basePrice = 10;
+var ingredients = {
+  pepperonni: {
+    name: 'Pepperonni',
+    price: 1
+  },
+  mushrooms: {
+    name: 'Mushrooms',
+    price: 1
+  },
+  greenPeppers: {
+    name: 'Green Peppers',
+    price: 1
+  },
+  whiteSauce: {
+    name: 'White sauce',
+    price: 3
+  },
+  glutenFreeCrust: {
+    name: 'Gluten-free crust',
+    price: 5
+  } // Initial value of the state (the state values can change over time)
+
+};
+var state = {
+  pepperonni: true,
+  mushrooms: true,
+  greenPeppers: true,
+  whiteSauce: false,
+  glutenFreeCrust: false // This function takes care of rendering the pizza based on the state
+  // This function is triggered once at the begining and everytime the state is changed
+
+};
+
+function renderEverything() {
+  renderPepperonni();
+  renderMushrooms();
+  renderGreenPeppers();
+  renderWhiteSauce();
+  renderGlutenFreeCrust();
+  renderButtons();
+  renderPrice();
+}
+
+function renderPepperonni() {
+  document.querySelectorAll('.pep').forEach(function ($pep) {
+    if (state.pepperonni) {
+      $pep.style.visibility = "visible";
+    } else {
+      $pep.style.visibility = "hidden";
+    }
+  });
+}
+
+function renderMushrooms() {
+  document.querySelectorAll('.mushroom').forEach(function ($mush) {
+    if (state.mushrooms) {
+      $mush.style.visibility = "visible";
+    } else {
+      $mush.style.visibility = "hidden";
+    }
+  });
+}
+
+function renderGreenPeppers() {
+  document.querySelectorAll('.green-pepper').forEach(function ($greenPepper) {
+    if (state.greenPeppers) {
+      $greenPepper.style.visibility = "visible";
+    } else {
+      $greenPepper.style.visibility = "hidden";
+    }
+  });
+}
+
+function renderWhiteSauce() {
+  document.querySelectorAll('.sauce').forEach(function ($sauce) {
+    if (state.whiteSauce) {
+      $sauce.classList.add('sauce-white');
+    } else {
+      $sauce.classList.remove('sauce-white');
+    }
+  });
+}
+
+function renderGlutenFreeCrust() {
+  document.querySelectorAll('.crust').forEach(function ($glutenFree) {
+    if (state.glutenFreeCrust) {
+      $glutenFree.classList.add('crust-gluten-free');
+    } else {
+      $glutenFree.classList.remove('crust-gluten-free');
+    }
+  });
+}
+
+function renderButtons() {
+  // Iteration 3: add/remove the class "active" of each `<button class="btn">`
+  var btns = document.querySelectorAll('.btn');
+  var objKeys = Object.keys(state);
+  btns.forEach(function (item, index) {
+    if (state[objKeys[index]]) {
+      item.classList.add('active');
+    } else {
+      item.classList.remove('active');
+    }
+
+    ;
+  });
+}
+
+function renderPrice() {
+  var objKeys = Object.keys(state); // Zerando o a tag ul
+
+  document.querySelectorAll('.panel + .price ul')[0].innerHTML = '';
+  var result = 10;
+  objKeys.forEach(function (ingredient) {
+    if (state[ingredient]) {
+      result += ingredients[ingredient].price;
+      var li = document.createElement('li'); // Criando node
+
+      li.innerHTML = "".concat(ingredients[ingredient].price, " ").concat(ingredient); // Atribuindo valor ao node
+
+      document.querySelectorAll('.panel + .price ul')[0].appendChild(li);
+    }
+  });
+  document.querySelectorAll('.panel + .price strong')[0].innerHTML = result; // Iteration 4: change the HTML of `<aside class="panel price">`
+}
+
+renderEverything(); // Iteration 1: Example of a click event listener on `<button class="btn btn-pepperonni">`
+
+document.querySelector('.btn.btn-pepperonni').onclick = function () {
+  state.pepperonni = !state.pepperonni;
+  renderEverything();
+}; // Iteration 1: Add click event listener on `<button class="btn btn-mushrooms">`
+
+
+document.querySelector('.btn.btn-mushrooms').onclick = function () {
+  state.mushrooms = !state.mushrooms;
+  renderEverything();
+}; // Iteration 1: Add click event listener on `<button class="btn btn-green-peppers">`
+
+
+document.querySelector('.btn.btn-green-peppers').onclick = function () {
+  state.greenPeppers = !state.greenPeppers;
+  renderEverything();
+}; // Iteration 2: Add click event listener on `<button class="btn btn-sauce">`
+
+
+document.querySelector('.btn.btn-sauce').onclick = function () {
+  state.whiteSauce = !state.whiteSauce;
+  renderEverything();
+}; // Iteration 2: Add click event listener on `<button class="btn btn-crust">`
+
+
+document.querySelector('.btn.btn-crust').onclick = function () {
+  state.glutenFreeCrust = !state.glutenFreeCrust;
+  renderEverything();
+};
+},{}],"../../../../../.nvm/versions/node/v12.9.1/lib/node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+var global = arguments[3];
+var OVERLAY_ID = '__parcel__error__overlay__';
+var OldModule = module.bundle.Module;
+
+function Module(moduleName) {
+  OldModule.call(this, moduleName);
+  this.hot = {
+    data: module.bundle.hotData,
+    _acceptCallbacks: [],
+    _disposeCallbacks: [],
+    accept: function (fn) {
+      this._acceptCallbacks.push(fn || function () {});
+    },
+    dispose: function (fn) {
+      this._disposeCallbacks.push(fn);
+    }
+  };
+  module.bundle.hotData = null;
+}
+
+module.bundle.Module = Module;
+var checkedAssets, assetsToAccept;
+var parent = module.bundle.parent;
+
+if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
+  var hostname = "" || location.hostname;
+  var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "42381" + '/');
+
+  ws.onmessage = function (event) {
+    checkedAssets = {};
+    assetsToAccept = [];
+    var data = JSON.parse(event.data);
+
+    if (data.type === 'update') {
+      var handled = false;
+      data.assets.forEach(function (asset) {
+        if (!asset.isNew) {
+          var didAccept = hmrAcceptCheck(global.parcelRequire, asset.id);
+
+          if (didAccept) {
+            handled = true;
+          }
+        }
+      }); // Enable HMR for CSS by default.
+
+      handled = handled || data.assets.every(function (asset) {
+        return asset.type === 'css' && asset.generated.js;
+      });
+
+      if (handled) {
+        console.clear();
+        data.assets.forEach(function (asset) {
+          hmrApply(global.parcelRequire, asset);
+        });
+        assetsToAccept.forEach(function (v) {
+          hmrAcceptRun(v[0], v[1]);
+        });
+      } else {
+        window.location.reload();
+      }
+    }
+
+    if (data.type === 'reload') {
+      ws.close();
+
+      ws.onclose = function () {
+        location.reload();
+      };
+    }
+
+    if (data.type === 'error-resolved') {
+      console.log('[parcel] ✨ Error resolved');
+      removeErrorOverlay();
+    }
+
+    if (data.type === 'error') {
+      console.error('[parcel] 🚨  ' + data.error.message + '\n' + data.error.stack);
+      removeErrorOverlay();
+      var overlay = createErrorOverlay(data);
+      document.body.appendChild(overlay);
+    }
+  };
+}
+
+function removeErrorOverlay() {
+  var overlay = document.getElementById(OVERLAY_ID);
+
+  if (overlay) {
+    overlay.remove();
+  }
+}
+
+function createErrorOverlay(data) {
+  var overlay = document.createElement('div');
+  overlay.id = OVERLAY_ID; // html encode message and stack trace
+
+  var message = document.createElement('div');
+  var stackTrace = document.createElement('pre');
+  message.innerText = data.error.message;
+  stackTrace.innerText = data.error.stack;
+  overlay.innerHTML = '<div style="background: black; font-size: 16px; color: white; position: fixed; height: 100%; width: 100%; top: 0px; left: 0px; padding: 30px; opacity: 0.85; font-family: Menlo, Consolas, monospace; z-index: 9999;">' + '<span style="background: red; padding: 2px 4px; border-radius: 2px;">ERROR</span>' + '<span style="top: 2px; margin-left: 5px; position: relative;">🚨</span>' + '<div style="font-size: 18px; font-weight: bold; margin-top: 20px;">' + message.innerHTML + '</div>' + '<pre>' + stackTrace.innerHTML + '</pre>' + '</div>';
+  return overlay;
+}
+
+function getParents(bundle, id) {
+  var modules = bundle.modules;
+
+  if (!modules) {
+    return [];
+  }
+
+  var parents = [];
+  var k, d, dep;
+
+  for (k in modules) {
+    for (d in modules[k][1]) {
+      dep = modules[k][1][d];
+
+      if (dep === id || Array.isArray(dep) && dep[dep.length - 1] === id) {
+        parents.push(k);
+      }
+    }
+  }
+
+  if (bundle.parent) {
+    parents = parents.concat(getParents(bundle.parent, id));
+  }
+
+  return parents;
+}
+
+function hmrApply(bundle, asset) {
+  var modules = bundle.modules;
+
+  if (!modules) {
+    return;
+  }
+
+  if (modules[asset.id] || !bundle.parent) {
+    var fn = new Function('require', 'module', 'exports', asset.generated.js);
+    asset.isNew = !modules[asset.id];
+    modules[asset.id] = [fn, asset.deps];
+  } else if (bundle.parent) {
+    hmrApply(bundle.parent, asset);
+  }
+}
+
+function hmrAcceptCheck(bundle, id) {
+  var modules = bundle.modules;
+
+  if (!modules) {
+    return;
+  }
+
+  if (!modules[id] && bundle.parent) {
+    return hmrAcceptCheck(bundle.parent, id);
+  }
+
+  if (checkedAssets[id]) {
+    return;
+  }
+
+  checkedAssets[id] = true;
+  var cached = bundle.cache[id];
+  assetsToAccept.push([bundle, id]);
+
+  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
+    return true;
+  }
+
+  return getParents(global.parcelRequire, id).some(function (id) {
+    return hmrAcceptCheck(global.parcelRequire, id);
+  });
+}
+
+function hmrAcceptRun(bundle, id) {
+  var cached = bundle.cache[id];
+  bundle.hotData = {};
+
+  if (cached) {
+    cached.hot.data = bundle.hotData;
+  }
+
+  if (cached && cached.hot && cached.hot._disposeCallbacks.length) {
+    cached.hot._disposeCallbacks.forEach(function (cb) {
+      cb(bundle.hotData);
+    });
+  }
+
+  delete bundle.cache[id];
+  bundle(id);
+  cached = bundle.cache[id];
+
+  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
+    cached.hot._acceptCallbacks.forEach(function (cb) {
+      cb();
+    });
+
+    return true;
+  }
+}
+},{}]},{},["../../../../../.nvm/versions/node/v12.9.1/lib/node_modules/parcel/src/builtins/hmr-runtime.js","pizza.js"], null)
+//# sourceMappingURL=/pizza.43529a85.js.map
--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -44,26 +44,83 @@ function renderPepperonni() {
 }
 
 function renderMushrooms() {
-  // Iteration 1: set the visibility of `<section class="mushroom">`
+  document.querySelectorAll('.mushroom').forEach(function($mush){
+    if (state.mushrooms) {
+      $mush.style.visibility = "visible";
+    }
+    else {
+      $mush.style.visibility = "hidden";
+    }
+  })
 }
 
 function renderGreenPeppers() {
-  // Iteration 1: set the visibility of `<section class="green-pepper">`
+  document.querySelectorAll('.green-pepper').forEach(function($greenPepper){
+    if (state.greenPeppers) {
+      $greenPepper.style.visibility = "visible";
+    }
+    else {
+      $greenPepper.style.visibility = "hidden";
+    }
+  })
 }
 
 function renderWhiteSauce() {
-  // Iteration 2: add/remove the class "sauce-white" of `<section class="sauce">`
+  document.querySelectorAll('.sauce').forEach(function($sauce){
+    if (state.whiteSauce) {
+      $sauce.classList.add('sauce-white');
+    } else {
+      $sauce.classList.remove('sauce-white');
+    }
+  })
+
 }
 
 function renderGlutenFreeCrust() {
-  // Iteration 2: add/remove the class "crust-gluten-free" of `<section class="crust">`
+  document.querySelectorAll('.crust').forEach(function($glutenFree){
+    if (state.glutenFreeCrust) {
+      $glutenFree.classList.add('crust-gluten-free');
+    } else {
+      $glutenFree.classList.remove('crust-gluten-free');
+    }
+  });
 }
+
 
 function renderButtons() {
   // Iteration 3: add/remove the class "active" of each `<button class="btn">`
+
+  let btns = document.querySelectorAll('.btn');
+  let objKeys = Object.keys(state);
+
+  btns.forEach((item, index) => {
+    if (state[objKeys[index]]) {
+      item.classList.add('active');
+    } else {
+      item.classList.remove('active');
+    };
+  });   
 }
 
 function renderPrice() {
+  let objKeys = Object.keys(state);
+
+  // Zerando o a tag ul
+  document.querySelectorAll('.panel + .price ul')[0].innerHTML = '';
+  let result = 10;
+
+  objKeys.forEach(ingredient => {
+    if (state[ingredient]) {
+      result += ingredients[ingredient].price;
+
+      let li = document.createElement('li'); // Criando node
+      li.innerHTML = `${ingredients[ingredient].price} ${ingredient}`; // Atribuindo valor ao node
+      document.querySelectorAll('.panel + .price ul')[0]
+      .appendChild(li);
+    }
+  });
+
+  document.querySelectorAll('.panel + .price strong')[0].innerHTML = result;
   // Iteration 4: change the HTML of `<aside class="panel price">`
 }
 
@@ -77,9 +134,25 @@ document.querySelector('.btn.btn-pepperonni').onclick = function() {
 }
 
 // Iteration 1: Add click event listener on `<button class="btn btn-mushrooms">`
+document.querySelector('.btn.btn-mushrooms').onclick = function() {
+  state.mushrooms = !state.mushrooms
+  renderEverything()
+}
 
 // Iteration 1: Add click event listener on `<button class="btn btn-green-peppers">`
+document.querySelector('.btn.btn-green-peppers').onclick = function() {
+  state.greenPeppers = !state.greenPeppers
+  renderEverything()
+}
 
 // Iteration 2: Add click event listener on `<button class="btn btn-sauce">`
+document.querySelector('.btn.btn-sauce').onclick = function() {
+  state.whiteSauce = !state.whiteSauce
+  renderEverything()
+}
 
 // Iteration 2: Add click event listener on `<button class="btn btn-crust">`
+document.querySelector('.btn.btn-crust').onclick = function() {
+  state.glutenFreeCrust = !state.glutenFreeCrust
+  renderEverything()
+}
