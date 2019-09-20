938eced75db2c71b896ebd480a193092b72bb6b8
Victor Nevola <victornevolapn@gmail.com>
2019-09-19 22:03:54 -0300

    Done

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,85 +1,109 @@
-// Write your Pizza Builder JavaScript in this file.
-
-// Constants 
-var basePrice = 10
+//Function that add or remove elements of the pizza.
+function toggleElement(className, toggleClass){
+  let list = document.querySelectorAll(className)
+  list.forEach(function(element){
+    element.classList.toggle(toggleClass);
+  });
+}
 var ingredients = {
   pepperonni: {name: 'Pepperonni', price: 1},
   mushrooms: {name: 'Mushrooms', price: 1},
   greenPeppers: {name: 'Green Peppers', price: 1},
-  whiteSauce: {name: 'White sauce', price: 3},
-  glutenFreeCrust: {name: 'Gluten-free crust', price: 5}
+  whiteSauce: {name: 'White Sauce', price: 3},
+  glutenFreeCrust: {name: 'Gluten-free Crust', price: 5}
 }
+//Function that render price in the panel according of the button clicked
+function renderPrice(button) {
+  let list = document.querySelector('.panel.price');
+  let idTotal = document.querySelector('#total');
+  let priceTotal = parseInt(idTotal.innerText.split('$')[1]);
+  let name, price, idName;
 
-// Initial value of the state (the state values can change over time)
-var state = {
-  pepperonni: true,
-  mushrooms: true,
-  greenPeppers: true,
-  whiteSauce: false,
-  glutenFreeCrust: false
-}
-
-// This function takes care of rendering the pizza based on the state
-// This function is triggered once at the begining and everytime the state is changed
-function renderEverything() {
-  renderPepperonni()
-  renderMushrooms()
-  renderGreenPeppers()
-  renderWhiteSauce()
-  renderGlutenFreeCrust()
-
-  renderButtons()
-  renderPrice()
-}
-
-function renderPepperonni() {
-  document.querySelectorAll('.pep').forEach(function($pep){
-    if (state.pepperonni) {
-      $pep.style.visibility = "visible";
+    //switch name, price and id
+    switch(true){
+      case button.classList.contains("btn-pepperonni"):
+        name = ingredients.pepperonni.name;
+        price = ingredients.pepperonni.price;
+        idName = 'pepperonni';
+      break;
+      case button.classList.contains("btn-mushrooms"):
+        name = ingredients.mushrooms.name;
+        price = ingredients.mushrooms.price;
+        idName = 'mushrooms';
+      break;
+      case button.classList.contains("btn-green-peppers"):
+        name = ingredients.greenPeppers.name;
+        price = ingredients.greenPeppers.price;
+        idName = 'green-peppers';
+      break;
+      case button.classList.contains("btn-sauce"):
+        name = ingredients.whiteSauce.name;
+        price = ingredients.whiteSauce.price;
+        idName = 'white-sauce';
+      break;
+      case button.classList.contains("btn-crust"):
+        name = ingredients.glutenFreeCrust.name;
+        price = ingredients.glutenFreeCrust.price;
+        idName = 'gluten-free-crust';
+      break;
     }
-    else {
-      $pep.style.visibility = "hidden";
+
+    //This part deletes or inserts if button is active or not.
+    if(button.classList.contains('active')){
+        let li = document.createElement('li');
+        li.setAttribute('id',idName);
+        li.innerText = `$${price} ${name}`
+        list.querySelector('ul').appendChild(li);
+        priceTotal += price
+        idTotal.innerText = `$${priceTotal}`;
     }
-  })
+    else{
+
+        let prices = list.querySelectorAll('li');
+      
+        prices.forEach(function(element){
+          if(element.id == idName){
+            element.remove();
+            priceTotal -= price;
+            idTotal.innerText = `$${priceTotal}`;
+          }
+        });
+    }
+ }
+
+
+// buttons that call the interactions.
+let pepperonniButton = document.querySelector('.btn-pepperonni');
+  pepperonniButton.onclick = function() {
+    pepperonniButton.classList.toggle('active');
+    toggleElement('.pep', 'hidden');
+    renderPrice(pepperonniButton);
 }
 
-function renderMushrooms() {
-  // Iteration 1: set the visibility of `<section class="mushroom">`
+let mushroomsButton = document.querySelector('.btn-mushrooms');
+  mushroomsButton.onclick = function(){
+    mushroomsButton.classList.toggle('active');
+    toggleElement('.mushroom', 'hidden');
+    renderPrice(mushroomsButton);
 }
 
-function renderGreenPeppers() {
-  // Iteration 1: set the visibility of `<section class="green-pepper">`
+let peppersButton = document.querySelector('.btn-green-peppers');
+  peppersButton.onclick = function(){
+    peppersButton.classList.toggle('active');
+    toggleElement('.green-pepper', 'hidden');
+    renderPrice(peppersButton);
 }
 
-function renderWhiteSauce() {
-  // Iteration 2: add/remove the class "sauce-white" of `<section class="sauce">`
+let sauceButton = document.querySelector('.btn-sauce');
+  sauceButton.onclick = function(){
+    sauceButton.classList.toggle('active');
+    toggleElement('.sauce', 'sauce-white');
+    renderPrice(sauceButton);
 }
 
-function renderGlutenFreeCrust() {
-  // Iteration 2: add/remove the class "crust-gluten-free" of `<section class="crust">`
+let crustButton = document.querySelector('.btn-crust');
+  crustButton.onclick = function(){
+    crustButton.classList.toggle('active');
+    toggleElement('.crust', 'crust-gluten-free');
+    renderPrice(crustButton);
 }
-
-function renderButtons() {
-  // Iteration 3: add/remove the class "active" of each `<button class="btn">`
-}
-
-function renderPrice() {
-  // Iteration 4: change the HTML of `<aside class="panel price">`
-}
-
-
-renderEverything()
-
-// Iteration 1: Example of a click event listener on `<button class="btn btn-pepperonni">`
-document.querySelector('.btn.btn-pepperonni').onclick = function() {
-  state.pepperonni = !state.pepperonni
-  renderEverything()
-}
-
-// Iteration 1: Add click event listener on `<button class="btn btn-mushrooms">`
-
-// Iteration 1: Add click event listener on `<button class="btn btn-green-peppers">`
-
-// Iteration 2: Add click event listener on `<button class="btn btn-sauce">`
-
-// Iteration 2: Add click event listener on `<button class="btn btn-crust">`
