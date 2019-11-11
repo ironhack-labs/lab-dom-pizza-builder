df638383ac4a4c7461137ab2c4f134a992fee803
Edgar Zea <cesszea@icloud.com>
2019-11-11 16:56:24 -0600

    Done by Ardash and Edgar

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -15,8 +15,8 @@ var state = {
   pepperonni: true,
   mushrooms: true,
   greenPeppers: true,
-  whiteSauce: true,
-  glutenFreeCrust: true
+  whiteSauce: false,
+  glutenFreeCrust: false
 }
 
 // This function takes care of rendering the pizza based on the state
@@ -69,41 +69,94 @@ function renderGreenPeppers() {
 
 function renderWhiteSauce() {
   // Iteration 2: add/remove the class "sauce-white" of `<section class="sauce">`
-  document.querySelectorAll('.sauce').forEach(function($sauceWhite){
-    if (state.whiteSauce) {
-      $sauceWhite.style.visibility = "visible";
-    }
-    else {
-      $sauceWhite.style.visibility = "hidden";
-    }
-  })
+  if (state.whiteSauce){
+   document.querySelector(".sauce").classList.add("sauce-white")
+ } else {
+   document.querySelector(".sauce").classList.remove("sauce-white")
+ }
 }
 
 function renderGlutenFreeCrust() {
   // Iteration 2: add/remove the class "crust-gluten-free" of `<section class="crust">`
-  document.querySelectorAll('.crust').forEach(function($gluten){
-    if (state.glutenFreeCrust) {
-      $gluten.style.visibility = "visible";
-    }
-    else {
-      $gluten.style.visibility = "hidden";
-    }
-  })
+  if (state.glutenFreeCrust){
+    document.querySelector(".crust").classList.add("crust-gluten-free")
+  } else {
+    document.querySelector(".crust").classList.remove("crust-gluten-free")
+  }
 }
 
 function renderButtons($btn) {
-  if (!$btn) return
-  // Iteration 3: add/remove the class "active" of each `<button class="btn">`
-  if ($btn.classList.contains('active')) {
-      $btn.classList.remove('active')
-    }
-    else {
-      $btn.classList.add('active')
-    }
+  if (state.glutenFreeCrust){
+    document.querySelector('.btn.btn-crust').className += ' active';
+  } else{
+    document.querySelector('.btn.btn-crust').classList.remove('active');
+  }
+  if (state.whiteSauce){
+    document.querySelector('.btn.btn-sauce').className += ' active';
+  } else{
+    document.querySelector('.btn.btn-sauce').classList.remove('active');
+  }
+  if (state.pepperonni){
+    document.querySelector('.btn.btn-pepperonni').className += ' active';
+  } else{
+    document.querySelector('.btn.btn-pepperonni').classList.remove('active');
+  }
+  if (state.mushrooms){
+    document.querySelector('.btn.btn-mushrooms').className += ' active';
+  } else{
+    document.querySelector('.btn.btn-mushrooms').classList.remove('active');
+  }
+  if (state.greenPeppers){
+    document.querySelector('.btn.btn-green-peppers').className += ' active';
+  } else{
+    document.querySelector('.btn.btn-green-peppers').classList.remove('active');
+  }
 }
 
 function renderPrice() {
   // Iteration 4: change the HTML of `<aside class="panel price">`
+  if (state.glutenFreeCrust){
+    document.querySelector('.panel.price ul li+li+li+li+li').innerText = '$5 gluten-free crust';
+  } else{
+    document.querySelector('.panel.price ul li+li+li+li+li').innerText = 'Gluten-free crust not selected';
+  }
+  if (state.whiteSauce){
+    document.querySelector('.panel.price ul li+li+li+li').innerText = '$3 white sauce';
+  } else{
+    document.querySelector('.panel.price ul li+li+li+li').innerText = 'White sauce not selected';
+  }
+  if (state.pepperonni){
+    document.querySelector('.panel.price ul li').innerText = '$1 pepperoni';
+  } else{
+    document.querySelector('.panel.price ul li').innerText = 'Pepperoni not selected';
+  }
+  if (state.mushrooms){
+    document.querySelector('.panel.price ul li+li').innerText = '$1 mushrooms';
+  } else{
+    document.querySelector('.panel.price ul li+li').innerText = 'Mushrooms not selected';
+  }
+  if (state.greenPeppers){
+    document.querySelector('.panel.price ul li+li+li').innerText = '$1 green peppers';
+  } else{
+    document.querySelector('.panel.price ul li+li+li').innerText = 'Green Peppers not selected';
+  }
+  let price = 10;
+  if (state.glutenFreeCrust){
+    price +=5
+  }
+  if (state.whiteSauce){
+    price +=3
+  }
+  if (state.pepperonni){
+    price +=1
+  }
+  if (state.mushrooms){
+    price +=1
+  }
+  if (state.greenPeppers){
+    price +=1
+  }
+  document.querySelector('.panel.price strong').innerText = `$${price}`
 }
 
 
