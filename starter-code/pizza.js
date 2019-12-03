41a646c7215a07900bbdba95412524e2fb0de3c3
Maxwell Martins Lima Monteiro <maxwba@AbelardosiPhone.localdomain>
2019-12-03 15:11:46 -0300

    1,2,3 Ready

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -20,7 +20,8 @@ var state = {
 }
 
 // This function takes care of rendering the pizza based on the state
-// This function is triggered once at the begining and everytime the state is changed
+// This function is triggered once at the begining and everytime 
+//the state is changed
 function renderEverything() {
   renderPepperonni()
   renderMushrooms()
@@ -45,34 +46,97 @@ function renderPepperonni() {
 
 function renderMushrooms() {
   // Iteration 1: set the visibility of `<section class="mushroom">`
+  document.querySelectorAll('.mushroom').forEach(function($mushroom){
+    if (state.mushrooms) {
+      $mushroom.style.visibility = "visible";
+    }
+    else {
+      $mushroom.style.visibility = "hidden";
+    }
+  })
 }
 
 function renderGreenPeppers() {
   // Iteration 1: set the visibility of `<section class="green-pepper">`
+  document.querySelectorAll('.green-pepper').forEach(function($greenPeper){
+    if (state.greenPeppers) {
+      $greenPeper.style.visibility = "visible";
+    }
+    else {
+      $greenPeper.style.visibility = "hidden";
+    }
+  })
 }
 
 function renderWhiteSauce() {
   // Iteration 2: add/remove the class "sauce-white" of `<section class="sauce">`
+  document.querySelectorAll('.sauce').forEach(function($sauceWhite){
+    if (state.sauceWhite) {
+      $sauceWhite.classList.add("sauce-white");
+    }
+    else {
+      $sauceWhite.classList.remove("sauce-white");
+    }
+  })
 }
 
 function renderGlutenFreeCrust() {
-  // Iteration 2: add/remove the class "crust-gluten-free" of `<section class="crust">`
+  document.querySelectorAll('.crust').forEach(function($crust){
+    if (state.glutenFreeCrust) {
+      $crust.classList.add("crust-gluten-free");
+    }
+    else {
+      $crust.classList.remove("crust-gluten-free");
+    }
+  })
 }
 
+
 function renderButtons() {
-  // Iteration 3: add/remove the class "active" of each `<button class="btn">`
+  if (state.pepperonni === true) {
+    document.querySelector('.btn-pepperonni.active').add
+  }
 }
+  //   .btn btn-pepperonni active
+//   if (se o botao for clicado){
+//     chamar a "classe btn btn-pepperonni active" que vai ativar o css
+//   }else {
+//     retire a classe btn btn-pepperonni active
+//   }
+// }
 
 function renderPrice() {
   // Iteration 4: change the HTML of `<aside class="panel price">`
+
 }
 
 
 renderEverything()
 
 // Iteration 1: Example of a click event listener on `<button class="btn btn-pepperonni">`
+
 document.querySelector('.btn.btn-pepperonni').onclick = function() {
   state.pepperonni = !state.pepperonni
+  renderEverything()
+}
+
+document.querySelector('.btn.btn-mushrooms').onclick = function() {
+  state.mushrooms = !state.mushrooms
+  renderEverything()
+}
+
+document.querySelector('.btn.btn-green-peppers').onclick = function() {
+  state.greenPeppers = !state.greenPeppers
+  renderEverything()
+}
+
+document.querySelector('.btn.btn-sauce').onclick = function() {
+  state.sauceWhite = !state.sauceWhite
+  renderEverything()
+}
+
+document.querySelector('.btn.btn-crust').onclick = function() {
+  state.glutenFreeCrust = !state.glutenFreeCrust
   renderEverything()
 }
 
