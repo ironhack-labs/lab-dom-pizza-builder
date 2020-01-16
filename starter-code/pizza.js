b056fe3875e47b078b76e576255e85210a9d2420
camilledlr <robinel.camille@gmail.com>
2020-01-16 09:31:19 +0100

    done

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -11,13 +11,6 @@ var ingredients = {
 }
 
 // Initial value of the state (the state values can change over time)
-var state = {
-  pepperonni: true,
-  mushrooms: true,
-  greenPeppers: true,
-  whiteSauce: false,
-  glutenFreeCrust: false
-}
 
 // This function takes care of rendering the pizza based on the state
 // This function is triggered once at the begining and everytime the state is changed
@@ -27,8 +20,7 @@ function renderEverything() {
   renderGreenPeppers()
   renderWhiteSauce()
   renderGlutenFreeCrust()
-
-  renderButtons()
+  // renderButtons()
   renderPrice()
 }
 
@@ -44,28 +36,72 @@ function renderPepperonni() {
 }
 
 function renderMushrooms() {
-  // Iteration 1: set the visibility of `<section class="mushroom">`
+  document.querySelectorAll('.mushroom').forEach(function(mush){
+    if (state.mushrooms) {
+     mush.style.visibility = "visible";
+    }
+    else {
+     mush.style.visibility = "hidden";
+    }
+  })
 }
 
 function renderGreenPeppers() {
-  // Iteration 1: set the visibility of `<section class="green-pepper">`
+  document.querySelectorAll('.green-pepper').forEach(function(green){
+    if (state.greenPeppers) {
+     green.style.visibility = "visible";
+    }
+    else {
+     green.style.visibility = "hidden";
+    }
+  })
 }
 
 function renderWhiteSauce() {
-  // Iteration 2: add/remove the class "sauce-white" of `<section class="sauce">`
-}
+  let sauce = document.querySelector(".sauce") ;
+  if (state.whiteSauce) {
+    sauce.classList.add("sauce-white");
+   }
+   else {
+    sauce.classList.remove("sauce-white");
+   }
+ }
+
 
 function renderGlutenFreeCrust() {
-  // Iteration 2: add/remove the class "crust-gluten-free" of `<section class="crust">`
+  let crust = document.querySelector(".crust")
+  if (state.glutenFreeCrust) {
+    crust.classList.add("crust-gluten-free");
+   }
+   else {
+    crust.classList.remove("crust-gluten-free");
+   }
 }
 
-function renderButtons() {
-  // Iteration 3: add/remove the class "active" of each `<button class="btn">`
+var state = {
+  pepperonni: true,
+  mushrooms: true,
+  greenPeppers: true,
+  whiteSauce: false,
+  glutenFreeCrust: false
 }
+
 
 function renderPrice() {
-  // Iteration 4: change the HTML of `<aside class="panel price">`
-}
+let price = 10 ;
+let ingList = document.querySelector("aside ul");
+ingList.innerHTML ="";
+  for (property in state) {
+    if (state[property]){ 
+        price += ingredients[property].price;
+      ingList.innerHTML += `<li>${ingredients[property].price} ${ingredients[property].name}`
+    } 
+      
+    }
+    document.getElementById("price").innerHTML = `${price}`;
+  }
+
+
 
 
 renderEverything()
@@ -73,13 +109,39 @@ renderEverything()
 // Iteration 1: Example of a click event listener on `<button class="btn btn-pepperonni">`
 document.querySelector('.btn.btn-pepperonni').onclick = function() {
   state.pepperonni = !state.pepperonni
+  this.classList.toggle("active");
   renderEverything()
 }
 
 // Iteration 1: Add click event listener on `<button class="btn btn-mushrooms">`
 
-// Iteration 1: Add click event listener on `<button class="btn btn-green-peppers">`
+document.querySelector('.btn-mushrooms').onclick = function() {
+  state.mushrooms = !state.mushrooms
+  this.classList.toggle("active");
+  renderEverything()
+}
+
+document.querySelector('.btn-green-peppers').onclick = function() {
+  state.greenPeppers = !state.greenPeppers
+  this.classList.toggle("active");
+  renderEverything()
+}
 
 // Iteration 2: Add click event listener on `<button class="btn btn-sauce">`
 
+document.querySelector('.btn-sauce').onclick = function() {
+  state.whiteSauce = !state.whiteSauce
+  this.classList.toggle("active");
+  renderEverything()
+}
+
 // Iteration 2: Add click event listener on `<button class="btn btn-crust">`
+
+document.querySelector(".btn-crust").onclick = function () {
+  state.glutenFreeCrust = !state.glutenFreeCrust
+  this.classList.toggle("active");
+  renderEverything()
+}
+
+
+
