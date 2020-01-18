f82f580031a7b796181417c7392bf3be86e1c6f4
Pablolo <pablo_olondriz@hotmail.com>
2020-01-18 21:04:23 +0100

    iteration1

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -44,11 +44,25 @@ function renderPepperonni() {
 }
 
 function renderMushrooms() {
-  // Iteration 1: set the visibility of `<section class="mushroom">`
+  document.querySelectorAll('.mushroom').forEach(function($mushroom){
+    if (state.mushroom) {
+      $mushroom.style.visibility = "visible";
+    }
+    else {
+      $mushroom.style.visibility = "hidden";
+    }
+  })
 }
 
 function renderGreenPeppers() {
-  // Iteration 1: set the visibility of `<section class="green-pepper">`
+  document.querySelectorAll('.green-pepper').forEach(function($greenpepper){
+    if (state.greenPeppers) {
+      $greenpepper.style.visibility = "visible";
+    }
+    else {
+      $greenpepper.style.visibility = "hidden";
+    }
+  })
 }
 
 function renderWhiteSauce() {
@@ -78,7 +92,17 @@ document.querySelector('.btn.btn-pepperonni').onclick = function() {
 
 // Iteration 1: Add click event listener on `<button class="btn btn-mushrooms">`
 
+document.querySelector('.btn.btn-mushrooms').onclick = function() {
+  state.mushroom = !state.mushroom
+  renderEverything()
+}
+
 // Iteration 1: Add click event listener on `<button class="btn btn-green-peppers">`
+
+document.querySelector('.btn.btn-green-peppers').onclick = function() {
+  state.greenPeppers = !state.greenPeppers
+  renderEverything()
+}
 
 // Iteration 2: Add click event listener on `<button class="btn btn-sauce">`
 
