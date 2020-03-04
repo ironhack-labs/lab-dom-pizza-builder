5aaa61a3690974329b022712f7c3678805b1ec77
Gabriel Bohn <gabrielcbohn@gmail.com>
2020-03-04 10:05:55 -0300

    Done

--- js/index.js
+++ js/index.js
@@ -87,21 +87,52 @@ function renderGlutenFreeCrust() {
 }
 
 function renderButtons() {
-  // Iteration 3: add/remove the class "active" of each `<button class="btn">`
-  document.querySelectorAll(".btn").forEach(btn => {
-    if (state.btn) {
-      btn.classList.add('.active');
-    } else {
-      btn.classList.remove('.active');
-    }
-  });
+  if(state.pepperoni)
+  {
+    document.querySelector('.btn-pepperoni').classList.add('active');
+  }
+  else{
+    document.querySelector('.btn-pepperoni').classList.remove('active');
+  }
+
+  if(state.greenPeppers)
+  {
+    document.querySelector('.btn-green-peppers').classList.add('active');
+  }
+  else{
+    document.querySelector('.btn-green-peppers').classList.remove('active');
+  }
+
+  if(state.mushrooms)
+  {
+    document.querySelector('.btn-mushrooms').classList.add('active');
+  }
+  else{
+    document.querySelector('.btn-mushrooms').classList.remove('active');
+  }
+
+  if(state.whiteSauce)
+  {
+    document.querySelector('.btn-sauce').classList.add('active');
+  }
+  else{
+    document.querySelector('.btn-sauce').classList.remove('active');
+  }
+
+  if(state.glutenFreeCrust)
+  {
+    document.querySelector('.btn-crust').classList.add('active');
+  }
+  else{
+    document.querySelector('.btn-crust').classList.remove('active');
+  }
 }
 
 function renderPrice() {
   // Iteration 4: change the HTML of `<aside class="panel price">`
   let counter = basePrice;
 
-  let parent = document.querySelector("aside.panel.price ul");
+  let parent = document.querySelector(".panel.price>ul");
   
   parent.innerHTML = "";
 
@@ -112,7 +143,7 @@ function renderPrice() {
       counter += condiments[cond].price;
       parent.innerHTML += `<li> $ ${condiments[cond].price} ${condiments[cond].name} </li>`;
     }
-    document.querySelector('aside.panel.price strong').innerHTML = `$` + counter;
+    document.querySelector('.panel.price>strong').innerHTML = `$` + counter;
   }
 }
 
