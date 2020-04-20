9b5e61065dccfd8ffae4de34da275881ac1abdb5
JesusSantana <jesus@esmarketingdigital.com>
2020-04-20 20:15:03 +0200

    v3.0 Make de buttons active or not

--- js/index.js
+++ js/index.js
@@ -37,85 +37,119 @@ let state = {
 // This function takes care of rendering the pizza based on the state
 // This function is triggered once at the beginning and every time the state is changed
 function renderEverything() {
+  renderIngredients();
+  renderButtons();
+  renderPrice();
+}
+
+function renderIngredients() {
   renderPepperoni();
   renderMushrooms();
   renderGreenPeppers();
   renderWhiteSauce();
   renderGlutenFreeCrust();
-
-  renderButtons();
-  renderPrice();
-}
-
-function renderIngredients(selectClass, stateName) {
-  document
-    .querySelectorAll(selectClass)
-    .forEach(
-      oneIngredient =>
-        (oneIngredient.style.visibility = stateName ? "visible" : "hidden")
-    );
 }
 
 function renderPepperoni() {
-  renderIngredients(".pep", state.pepperoni);
+  renderElements(".pep", "pepperoni");
 }
 
 function renderMushrooms() {
-  // Iteration 1: set the visibility of `<section class='mushroom'>`
-  renderIngredients(".mushroom", state.mushrooms);
+  // Iteration 1: set the visibility of `<section class="mushroom">`
+  renderElements(".mushroom", "mushrooms");
 }
 
 function renderGreenPeppers() {
-  // Iteration 1: set the visibility of `<section class='green-pepper'>`
-  renderIngredients(".green-pepper", state.greenPeppers);
+  // Iteration 1: set the visibility of `<section class="green-pepper">`
+  renderElements(".green-pepper", "greenPeppers");
 }
 
 function renderWhiteSauce() {
-  // Iteration 2: add/remove the class 'sauce-white' of `<section class='sauce'>`
-  renderIngredients(".sauce", state.whiteSauce);
+  // Iteration 2: add/remove the class "sauce-white" of `<section class="sauce">`
+  renderPizzaBase(".sauce", "whiteSauce", "sauce-white");
 }
 
 function renderGlutenFreeCrust() {
-  // Iteration 2: add/remove the class 'crust-gluten-free' of `<section class='crust'>`
-  renderIngredients(".crust-gluten-free", state.glutenFreeCrust);
+  // Iteration 2: add/remove the class "crust-gluten-free" of `<section class="crust">`
+  renderPizzaBase(".crust", "glutenFreeCrust", "crust-gluten-free");
+}
+
+function renderElements(ingredientClass, ingredientState) {
+  document
+    .querySelectorAll(ingredientClass)
+    .forEach(ingredient =>
+      state[ingredientState]
+        ? (ingredient.style.visibility = "visible")
+        : (ingredient.style.visibility = "hidden")
+    );
+}
+
+function renderPizzaBase(ingredientClass, ingredient, changeElement) {
+  !state[ingredient]
+    ? document.querySelector(ingredientClass).classList.remove(changeElement)
+    : document.querySelector(ingredientClass).classList.add(changeElement);
 }
 
 function renderButtons() {
-  // Iteration 3: add/remove the class 'active' of each `<button class='btn'>`
+  // Iteration 3: add/remove the class "active" of each `<button class="btn">`
+  const buttonState = Object.keys(state);
+  document.querySelectorAll(".btn").forEach((button, i) => {
+    !state[buttonState[i]]
+      ? button.classList.remove("active")
+      : button.classList.add("active");
+  });
 }
 
-function renderPrice() {
-  // Iteration 4: change the HTML of `<aside class='panel price'>`
-}
+function renderPrice() {}
 
 renderEverything();
 
-// Iteration 1: Example of a click event listener on `<button class='btn btn-pepperoni'>`
+// Iteration 1: Example of a click event listener on `<button class="btn btn-pepperoni">`
 document.querySelector(".btn.btn-pepperoni").addEventListener("click", () => {
   state.pepperoni = !state.pepperoni;
   renderEverything();
 });
-
-// Iteration 1: Add click event listener on `<button class='btn btn-mushrooms'>`
-document.querySelector(".btn.btn-mushroom").addEventListener("click", () => {
+// Iteration 1: Add click event listener on `<button class="btn btn-mushrooms">`
+document.querySelector(".btn.btn-mushrooms").addEventListener("click", () => {
   state.mushrooms = !state.mushrooms;
   renderEverything();
 });
-
-// Iteration 1: Add click event listener on `<button class='btn btn-green-peppers'>`
-document.querySelector(".btn.btn-green-pepper").addEventListener("click", () => {
+// Iteration 1: Add click event listener on `<button class="btn btn-green-peppers">`
+document
+  .querySelector(".btn.btn-green-peppers")
+  .addEventListener("click", () => {
     state.greenPeppers = !state.greenPeppers;
     renderEverything();
-});
-
-// Iteration 2: Add click event listener on `<button class='btn btn-sauce'>`
-document.querySelector(".btn.btn-btn-sauce").addEventListener("click", () => {
+  });
+// Iteration 2: Add click event listener on `<button class="btn btn-sauce">`
+document.querySelector(".btn.btn-sauce").addEventListener("click", () => {
   state.whiteSauce = !state.whiteSauce;
   renderEverything();
 });
-
-// Iteration 2: Add click event listener on `<button class='btn btn-crust'>`
-document.querySelector(".btn.btn-btn-crust").addEventListener("click", () => {
+// Iteration 2: Add click event listener on `<button class="btn btn-crust">`
+document.querySelector(".btn.btn-crust").addEventListener("click", () => {
   state.glutenFreeCrust = !state.glutenFreeCrust;
   renderEverything();
 });
+
+/*function addNewEvent(selectClass, stateName) {
+  document.querySelector(selectClass).addEventListener("click", () => {
+    state[stateName] = !state[stateName];
+    renderEverything();
+  });
+}
+
+// Iteration 1: Example of a click event listener on `<button class='btn btn-pepperoni'>`
+addNewEvent('btn-pepperoni', pepperoni);
+
+// Iteration 1: Add click event listener on `<button class='btn btn-mushrooms'>`
+addNewEvent('.btn-mushroom', mushrooms);
+
+// Iteration 1: Add click event listener on `<button class='btn btn-green-peppers'>`
+addNewEvent('btn-green-peppers', greenPeppers);
+
+// Iteration 2: Add click event listener on `<button class='btn btn-sauce'>`
+addNewEvent('.btn-sauce', whiteSauce);
+
+// Iteration 2: Add click event listener on `<button class='btn btn-crust'>`
+addNewEvent('.btn-crust', glutenFreeCrust);*/
