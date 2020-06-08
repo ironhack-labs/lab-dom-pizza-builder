15c7bfb2a47febfe5e388f3d328255f79021c077
LUPOC <lupo.cordero@gmail.com>
2020-06-08 20:52:54 +0200

    Finished almost all except the easiest part

--- js/index.js
+++ js/index.js
@@ -43,28 +43,93 @@ function renderPepperoni() {
 }
 
 function renderMushrooms() {
-  // Iteration 1: set the visibility of `<section class="mushroom">`
+  document.querySelectorAll('.mushroom').forEach(oneMush => {
+    if (state.mushrooms) {
+      oneMush.style.visibility = 'visible';
+    } else {
+      oneMush.style.visibility = 'hidden';
+    }
+  });
 }
 
 function renderGreenPeppers() {
-  // Iteration 1: set the visibility of `<section class="green-pepper">`
+  document.querySelectorAll('.green-pepper').forEach(oneGreen => {
+    if (state.greenPeppers) {
+      oneGreen.style.visibility = 'visible';
+    } else {
+      oneGreen.style.visibility = 'hidden';
+    }
+  });
 }
 
 function renderWhiteSauce() {
-  // Iteration 2: add/remove the class "sauce-white" of `<section class="sauce">`
-}
+    document.querySelectorAll('.sauce').forEach(white => {
+      if (state.whiteSauce) {
+        white.classList.add('sauce-white');
+      } else {
+        white.classList.remove('sauce-white');
+      }
+    });
+  }
 
 function renderGlutenFreeCrust() {
-  // Iteration 2: add/remove the class "crust-gluten-free" of `<section class="crust">`
+  document.querySelectorAll('.crust').forEach(gluten => {
+    if (state.glutenFreeCrust) {
+      gluten.classList.add("crust-gluten-free");
+    } else {
+      gluten.classList.remove("crust-gluten-free");
+    }
+  });
 }
 
 function renderButtons() {
   // Iteration 3: add/remove the class "active" of each `<button class="btn">`
+
+    if(state.pepperoni) {
+      document.querySelector('.btn-pepperoni').classList.add("active");
+    } else {
+      document.querySelector('.btn-pepperoni').classList.remove("active");
+    }
+    if(state.mushrooms) {
+      document.querySelector('.btn-mushrooms').classList.add("active");
+    } else {
+      document.querySelector('.btn-mushrooms').classList.remove("active");
+    }
+    if(state.greenPeppers) {
+      document.querySelector('.btn-green-peppers').classList.add("active");
+    } else {
+      document.querySelector('.btn-green-peppers').classList.remove("active");
+    }
+    if(state.whiteSauce) {
+      document.querySelector('.btn-sauce').classList.add("active");
+    } else {
+      document.querySelector('.btn-sauce').classList.remove("active");
+    }
+    if(state.pepperoni) {
+      document.querySelector('.btn-crust').classList.add("active");
+    } else {
+      document.querySelector('.btn-crust').classList.remove("active");
+}
+
 }
 
 function renderPrice() {
   // Iteration 4: change the HTML of `<aside class="panel price">`
+  basePrice=10
+  
+    for(let key of Object.keys(state)){
+    if(state[key]){
+      basePrice+=ingredients[key].price
+      document.getElementById(key).style.visibility="visible"
+    }
+    else{
+      document.getElementById(key).style.visibility="hidden"
+
+    }
+    
 }
+}
+
 
 renderEverything();
 
@@ -75,9 +140,22 @@ document.querySelector('.btn.btn-pepperoni').addEventListener('click', () => {
 });
 
 // Iteration 1: Add click event listener on `<button class="btn btn-mushrooms">`
-
+document.querySelector('.btn.btn-mushrooms').addEventListener('click', () => {
+  state.mushrooms = !state.mushrooms;
+  renderEverything();
+});
 // Iteration 1: Add click event listener on `<button class="btn btn-green-peppers">`
-
+document.querySelector('.btn-green-peppers').addEventListener('click', () => {
+  state.greenPeppers = !state.greenPeppers;
+  renderEverything();
+});
 // Iteration 2: Add click event listener on `<button class="btn btn-sauce">`
-
+ document.querySelector('.btn-sauce').addEventListener('click', () => {
+  state.whiteSauce = !state.whiteSauce;
+  renderEverything();
+});
 // Iteration 2: Add click event listener on `<button class="btn btn-crust">`
+  document.querySelector('.btn-crust').addEventListener('click', () => {
+  state.glutenFreeCrust = !state.glutenFreeCrust;
+  renderEverything();
+  })
