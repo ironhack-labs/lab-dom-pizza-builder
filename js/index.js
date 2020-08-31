762a6c5bc28ab26b3b5688edaf916b06a1abc385
AyloSrd <aylo.srd@gmail.com>
2020-08-31 16:16:06 +0200

    iteration-1 done

--- js/index.js
+++ js/index.js
@@ -43,6 +43,7 @@ function renderMushrooms() {
 
 function renderGreenPeppers() {
   // Iteration 1: set the visibility of `<section class="green-pepper">`
+  document.querySelectorAll('.green-pepper').forEach(oneGreenPep => state.greenPeppers ? oneGreenPep.style.visibility = 'visible' : oneGreenPep.style.visibility = 'hidden')
 }
 
 function renderWhiteSauce() {
@@ -73,8 +74,13 @@ document.querySelector('.btn.btn-pepperoni').addEventListener('click', () => {
 document.querySelector('.btn.btn-mushrooms').addEventListener('click', () => {
   state.mushrooms = !state.mushrooms
   renderEverything()
-});
+})
 // Iteration 1: Add click event listener on `<button class="btn btn-green-peppers">`
+document.querySelector('.btn.btn-green-peppers').addEventListener('click', () => {
+  state.greenPeppers = !state.greenPeppers
+  renderEverything()
+})
+
 
 // Iteration 2: Add click event listener on `<button class="btn btn-sauce">`
 
