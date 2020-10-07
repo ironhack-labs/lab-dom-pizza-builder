694b974e02e9c8a04d6bfde8a9ded09e2105ed1b
Lexirem <me.vallribera@gmail.com>
2020-10-07 19:23:43 +0200

    iter 2

--- js/index.js
+++ js/index.js
@@ -66,14 +66,24 @@ function renderGreenPeppers() {
 
 function renderWhiteSauce() {
   // Iteration 2: add/remove the class "sauce-white" of `<section class="sauce">`
-  document.querySelector('.sauce sauce-white') 
+  let sauce = document.querySelector('.sauce');
     if (state.whiteSauce){
-
+      sauce.classList.add('sauce-white');
+    } else {
+      sauce.classList.remove('sauce-white');
     } 
-}
+  }
+
+// currentState.classList.remove("sauce-white");
 
 function renderGlutenFreeCrust() {
   // Iteration 2: add/remove the class "crust-gluten-free" of `<section class="crust">`
+  let crust = document.querySelector('.crust');
+  if (state.glutenFreeCrust){
+    crust.classList.add('crust-gluten-free');
+  } else {
+    crust.classList.remove('crust-gluten-free')
+  }
 }
 
 function renderButtons() {
@@ -105,5 +115,13 @@ document.querySelector('.btn.btn-green-peppers').addEventListener('click', () =>
 })
 
 // Iteration 2: Add click event listener on `<button class="btn btn-sauce">`
+document.querySelector('.btn.btn-sauce').addEventListener('click', () => {
+  state.whiteSauce = !state.whiteSauce;
+  renderWhiteSauce();
+})
 
 // Iteration 2: Add click event listener on `<button class="btn btn-crust">`
+document.querySelector('.btn.btn-crust').addEventListener('click', () => {
+  state.glutenFreeCrust = !state.glutenFreeCrust;
+  renderGlutenFreeCrust();
+})
