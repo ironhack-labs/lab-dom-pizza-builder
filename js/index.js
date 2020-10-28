e9f888a918d783e7b3be56804a010d991a030ee2
Sergio <srosduran@gmail.com>
2020-10-28 19:46:47 +0100

    done

--- js/index.js
+++ js/index.js
@@ -43,27 +43,89 @@ function renderPepperoni() {
 }
 
 function renderMushrooms() {
-  // Iteration 1: set the visibility of `<section class="mushroom">`
+  document.querySelectorAll('.mushroom').forEach(oneMushroom => {
+    if (state.mushrooms) {
+      oneMushroom.style.visibility = 'visible';
+    } else {
+      oneMushroom.style.visibility = 'hidden';
+    }
+  });
 }
 
 function renderGreenPeppers() {
-  // Iteration 1: set the visibility of `<section class="green-pepper">`
+  document.querySelectorAll('.green-pepper').forEach(oneGreenPep => {
+    if (state.greenPeppers) {
+      oneGreenPep.style.visibility = 'visible';
+    } else {
+      oneGreenPep.style.visibility = 'hidden';
+    }
+  });
 }
 
 function renderWhiteSauce() {
-  // Iteration 2: add/remove the class "sauce-white" of `<section class="sauce">`
-}
+  const target = document.querySelector('.sauce-white')
+    if (state.whiteSauce) {
+      target.style.visibility = 'visible';
+    } else {
+      target.style.visibility = 'hidden';
+    }
+  };
+
 
 function renderGlutenFreeCrust() {
-  // Iteration 2: add/remove the class "crust-gluten-free" of `<section class="crust">`
-}
+  const target = document.querySelector('.crust')
+    if (state.glutenFreeCrust) {
+      target.classList.add('crust-gluten-free')
+    } else {
+      target.classList.remove('crust-gluten-free')
+    }
+  };
 
 function renderButtons() {
-  // Iteration 3: add/remove the class "active" of each `<button class="btn">`
+  const target = document.getElementsByClassName('btn')
+  const targetArr = [...target]
+
+  
+  targetArr.forEach((button)=>{
+    button.onclick = ()=>{
+        if(button.classList.contains('active')){
+          button.classList.remove('active')
+        } else {
+          button.classList.add('active')
+        }
+      }
+  })
 }
+
 
 function renderPrice() {
   // Iteration 4: change the HTML of `<aside class="panel price">`
+  let listHtml = ''
+  let totalPrice = 10
+  if(state.pepperoni){
+    listHtml += `<li> $${ingredients.pepperoni.price} ${ingredients.pepperoni.name} </li> `
+    totalPrice += 1
+  }
+  if(state.mushrooms){
+    listHtml = listHtml + `<li> $${ingredients.mushrooms.price} ${ingredients.mushrooms.name} </li> `
+    totalPrice += 1
+  }
+  if(state.greenPeppers){
+    listHtml = listHtml + `<li> $${ingredients.greenPeppers.price} ${ingredients.greenPeppers.name} </li> `
+    totalPrice += 1
+  }
+  if(state.whiteSauce){
+    listHtml = listHtml + `<li> $${ingredients.whiteSauce.price} ${ingredients.whiteSauce.name} </li> `
+    totalPrice += 3
+  }
+  if(state.glutenFreeCrust){
+    listHtml = listHtml + `<li> $${ingredients.glutenFreeCrust.price} ${ingredients.glutenFreeCrust.name} </li> `
+    totalPrice += 5
+  }
+  document.querySelector('.panel.price ul').innerHTML = listHtml
+  document.querySelector('.panel.price strong').innerText = `$ ${totalPrice}`
+
+
 }
 
 renderEverything();
@@ -75,9 +137,22 @@ document.querySelector('.btn.btn-pepperoni').addEventListener('click', () => {
 });
 
 // Iteration 1: Add click event listener on `<button class="btn btn-mushrooms">`
-
+document.querySelector('.btn.btn-mushrooms').addEventListener('click', () => {
+  state.mushrooms = !state.mushrooms;
+  renderEverything();
+});
 // Iteration 1: Add click event listener on `<button class="btn btn-green-peppers">`
-
+document.querySelector('.btn.btn-green-peppers').addEventListener('click', () => {
+  state.greenPeppers = !state.greenPeppers;
+  renderEverything();
+});
 // Iteration 2: Add click event listener on `<button class="btn btn-sauce">`
-
+document.querySelector('.btn.btn-sauce').addEventListener('click', () => {
+  state.whiteSauce = !state.whiteSauce;
+  renderEverything();
+});
 // Iteration 2: Add click event listener on `<button class="btn btn-crust">`
+document.querySelector('.btn.btn-crust').addEventListener('click', () => {
+  state.glutenFreeCrust = !state.glutenFreeCrust;
+  renderEverything();
+});
