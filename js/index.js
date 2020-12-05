2b1b18fe6b4b307076e4b79d30718f47af3064b1
Pedro Frias <plfriasf@gmail.com>
2020-12-05 04:32:03 -0800

    Iteration #1 done

--- js/index.js
+++ js/index.js
@@ -43,10 +43,24 @@ function renderPepperoni() {
 }
 
 function renderMushrooms() {
+  document.querySelectorAll(".mushroom").forEach((oneMush) => {
+    if (state.mushrooms) {
+      oneMush.style.visibility = "visible";
+    } else {
+      oneMush.style.visibility = "hidden";
+    }
+  });
   // Iteration 1: set the visibility of `<section class="mushroom">`
 }
 
 function renderGreenPeppers() {
+    document.querySelectorAll(".green-pepper").forEach((oneGreenPep) => {
+      if (state.greenPeppers) {
+        oneGreenPep.style.visibility = "visible";
+      } else {
+        oneGreenPep.style.visibility = "hidden";
+      }
+    });
   // Iteration 1: set the visibility of `<section class="green-pepper">`
 }
 
@@ -75,9 +89,16 @@ document.querySelector('.btn.btn-pepperoni').addEventListener('click', () => {
 });
 
 // Iteration 1: Add click event listener on `<button class="btn btn-mushrooms">`
-
+document.querySelector(".btn.btn-mushrooms").addEventListener("click", () => {
+  state.mushrooms = !state.mushrooms;
+  renderEverything();
+});
 // Iteration 1: Add click event listener on `<button class="btn btn-green-peppers">`
-
+document
+  .querySelector(".btn.btn-green-peppers").addEventListener("click", () => {
+    state.greenPeppers = !state.greenPeppers;
+    renderEverything();
+  });
 // Iteration 2: Add click event listener on `<button class="btn btn-sauce">`
 
 // Iteration 2: Add click event listener on `<button class="btn btn-crust">`
