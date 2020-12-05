4b559ca5e381087446e77058fc794438c2350de1
Manuel Carrillo Almoguera <manucaralmo@gmail.com>
2020-12-05 13:58:13 +0100

    Iteration 2 done

--- js/index.js
+++ js/index.js
@@ -55,10 +55,23 @@ function renderMushrooms() {
 
 function renderGreenPeppers() {
   // Iteration 1: set the visibility of `<section class="green-pepper">`
+  document.querySelectorAll('.green-pepper').forEach(oneGreenPepper => {
+    if (state.greenPeppers) {
+      oneGreenPepper.style.visibility = 'visible';
+    } else {
+      oneGreenPepper.style.visibility = 'hidden';
+    }
+  });
 }
 
 function renderWhiteSauce() {
   // Iteration 2: add/remove the class "sauce-white" of `<section class="sauce">`
+  let wSouce = document.querySelector('.sauce');
+    if (state.whiteSauce) {
+      wSouce.classList.add('sauce-white');
+    } else {
+      wSouce.classList.remove('sauce-white');
+    }
 }
 
 function renderGlutenFreeCrust() {
@@ -86,8 +99,17 @@ document.querySelector('.btn.btn-mushrooms').addEventListener('click', () => {
   state.mushrooms = !state.mushrooms;
   renderEverything();
 });
+
 // Iteration 1: Add click event listener on `<button class="btn btn-green-peppers">`
+document.querySelector('.btn.btn-green-peppers').addEventListener('click', () => {
+  state.greenPeppers = !state.greenPeppers;
+  renderEverything();
+});
 
 // Iteration 2: Add click event listener on `<button class="btn btn-sauce">`
+document.querySelector('.btn.btn-sauce').addEventListener('click', () => {
+  state.whiteSauce = !state.whiteSauce;
+  renderEverything();
+});
 
 // Iteration 2: Add click event listener on `<button class="btn btn-crust">`
