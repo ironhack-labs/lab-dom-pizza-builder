4ddf23f8b4ae6e35806eb18a4f3c3aa5d0760bea
Kseniia Ernest <hramtcovakseniia@gmail.com>
2023-04-26 19:04:29 -0400

    Iteration 2 complete

--- js/index.js
+++ js/index.js
@@ -66,10 +66,19 @@ function renderGreenPeppers() {
 
 function renderWhiteSauce() {
   // Iteration 2: add/remove the class "sauce-white" of `<section class="sauce">`
+  if (state.whiteSauce) {
+    document.querySelector('.sauce').classList.add('sauce-white');
+  } else {
+    document.querySelector('.sauce').classList.remove('sauce-white');
+  }
 }
 
 function renderGlutenFreeCrust() {
   // Iteration 2: add/remove the class "crust-gluten-free" of `<section class="crust">`
+  if (state.glutenFreeCrust) {
+    document.querySelector('.crust').classList.add('crust-gluten-free');
+  } else {document.querySelector('.crust').classList.remove('crust-gluten-free');
+}
 }
 
 function renderButtons() {
@@ -101,5 +110,13 @@ document.querySelector('.btn.btn-green-peppers').addEventListener('click', () =>
 });
 
 // Iteration 2: Add click event listener on `<button class="btn btn-sauce">`
+document.querySelector('.btn.btn-sauce').addEventListener('click', () => {
+  state.whiteSauce = !state.whiteSauce;
+  renderEverything();
+})
 
 // Iteration 2: Add click event listener on `<button class="btn btn-crust">`
+document.querySelector('.btn.btn-crust').addEventListener('click', () => {
+  state.glutenFreeCrust = !state.glutenFreeCrust;
+  renderEverything();
+})
