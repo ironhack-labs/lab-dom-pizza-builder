7f24aae7b6c4fcc82b946444925b8148832cd42c
Kseniia Ernest <hramtcovakseniia@gmail.com>
2023-04-27 18:17:43 -0400

    Iteration 4 complete

--- js/index.js
+++ js/index.js
@@ -109,6 +109,18 @@ function renderButtons() {
 
 function renderPrice() {
   // Iteration 4: change the HTML of `<aside class="panel price">`
+  let totalPrice = basePrice;
+  const $list = document.querySelector('aside.panel.price ul');
+  $list.innerHTML = '';
+
+  for (let ingredientKey in ingredients) {
+    if (state[ingredientKey]) {
+      totalPrice += ingredients[ingredientKey].price
+      $list.innerHTML += `<li>$${ingredients[ingredientKey].price} ${ingredients[ingredientKey].name.toLowerCase()}</li>` 
+    }
+  }
+ document.querySelector('aside.panel.price strong').innerHTML = '$' + totalPrice;
+
 }
 
 renderEverything();
